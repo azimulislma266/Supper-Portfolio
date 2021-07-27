652f4f01043463d082d2e3590b72bb5b4810bc0e
azimulislma266 <86977364+azimulislma266@users.noreply.github.com>
2021-07-27 15:22:22 +0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,25 @@
+                  
+                //   (function() {
+
+                //         var $gallery = new SimpleLightbox('.gallery a', {});
+
+                //       })('big');
+// As A Vanilla JavaScript Plugin
+
+var gallery =new SimpleLightbox('.gallery a', {
+
+    /* options */
+
+});
+
+ 
+
+// As A jQuery Plugin -->
+
+var gallery = $('.gallery a').simpleLightbox({
+
+    /* options */
+
+});
+
+
